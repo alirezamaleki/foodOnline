cfc1cbda1b85b7cf200b48806087623134649491
Ali Maleki <amaleki@ea.com>
2024-01-31 06:35:00 -0800

    added password reset features

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -137,3 +137,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+
+# SMTP Email Settings
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
