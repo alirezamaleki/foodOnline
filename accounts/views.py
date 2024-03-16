a5f58435043a3e25033c9466d6825eb946b99fda
Ali Maleki <amaleki@ea.com>
2024-03-16 05:17:30 -0700

    marketplace version0.1

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 
 from django.core.exceptions import PermissionDenied
+from django.template.defaultfilters import slugify
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -30,7 +31,7 @@ def check_role_customer(user):
 def registerUser(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in!')
-        return redirect('dashboard')
+        return redirect('myAccount')
     elif request.method == 'POST':
         print(request.POST)
         form = UserForm(request.POST)
@@ -75,7 +76,7 @@ def registerUser(request):
 def registerVendor(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in!')
-        return redirect('dashboard')
+        return redirect('myAccount')
     elif request.method == 'POST':
         # store the data and create the user
         form = UserForm(request.POST)
@@ -91,6 +92,8 @@ def registerVendor(request):
             user.save()
             vendor = v_form.save(commit=False) # don't commit yet because we need to clarify user and user_profile otherwise it'll be error
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'accounts',
     'vendor',
     'menu',
+    'marketplace',
 ]
 
 MIDDLEWARE = [
