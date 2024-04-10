072d02d0de877596c64c1607f087da5313cc8877
Ali Maleki <amaleki@ea.com>
2024-04-10 05:33:14 -0700

    Prepopulate Forms

--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
+from accounts.models import UserProfile
 
 
 @login_required(login_url='login')
 def cprofile(request):
-    profile_form = UserProfileForm()
-    user_form = UserInfoForm()
+    profile = get_object_or_404(UserProfile, user=request.user)
+    profile_form = UserProfileForm(instance=profile)
+    user_form = UserInfoForm(instance=request.user)
 
     context = {
         'profile_form': profile_form,
